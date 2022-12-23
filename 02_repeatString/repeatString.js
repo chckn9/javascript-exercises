e5b3c52d4e274a705790318dfd490d203993d3b0
chckn9 <thomasyeoh9@gmail.com>
2022-12-23 15:35:25 -0800

    Complete exercise 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, iterations) {
+    let word = '';
+    for (let i = 0; i < iterations; i++) {
+        word += string;
+    }
+    return word;
 };
 
 // Do not edit below this line
