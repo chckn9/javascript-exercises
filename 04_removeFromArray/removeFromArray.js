da8d5878b4e6d48839181e9e5955023ca45f3d3c
chckn9 <thomasyeoh9@gmail.com>
2022-12-23 22:21:03 -0800

    Complete exercise 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function(...args) {
+    const array = args[0];
 
+    for (let i = 0; i < args.length - 1; i++) {
+        let index = array.indexOf(args[i + 1]);
+
+        // Only splice if item is found
+        if (index > -1) {
+            array.splice(index, 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
