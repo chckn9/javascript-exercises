fd4c894c9b9fb7c2554a6b9bcfea1caeaf6f29c9
chckn9 <thomasyeoh9@gmail.com>
2022-12-28 16:29:33 -0800

    Complete exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,20 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    // Append letters only to new string
+    let newStr = "";
+    for (let i = 0; i < str.length; i++) {
+        if ((/[a-zA-Z]/).test(str.charAt(i)))
+            newStr += str.charAt(i);
+    }
 
+    // Change all to lowercase
+    newStr = newStr.toUpperCase();
+
+    // Determine whether it is a palindrome
+    for (let i = 0; i < newStr.length / 2; i++) {
+        if (newStr.charAt(i) !== newStr.charAt(newStr.length - i - 1))
+            return false;
+    }
+    return true;
 };
 
 // Do not edit below this line
