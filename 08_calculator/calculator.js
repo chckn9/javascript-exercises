5c4921f279d65e63be681e6b0b1e91040498a072
chckn9 <thomasyeoh9@gmail.com>
2022-12-28 16:11:05 -0800

    Complete exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sum = 0;
+  for (let number of array) {
+    sum += number;
+  }
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let sum = 1;
+  for (let number of array) {
+    sum *= number;
+  }
+  return sum;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+  let sum = 1;
+  for (let i = 1; i <= num; i++)
+    sum *= i;
+  return sum;
 };
 
 // Do not edit below this line
